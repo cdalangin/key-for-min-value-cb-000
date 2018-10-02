fa36336540ed8aea55174091a5ff828b316aaa15
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-02 13:41:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
   array = name_hash.to_a
-  if array[0][1] > array[1][1]
-    return array[1]
+  if (array[0][1] > array[1][1]) && (array[2][1] > array[1][1])
+    return array[1][0]
+  elsif (array[0][1] < array[1][1]) && (array[0][1] < array[2][1])
+    return array[0]
+  e
   end
 end
 
